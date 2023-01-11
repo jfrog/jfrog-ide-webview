05bf5da0791e6b39b1a9ffca1d66a7107b35966c
Or Geva <orgab@jfrog.com>
2023-01-11 18:02:53 +0200

    Add ZeroDays in Event Listener

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,25 @@ import Dependency from './components/Page/Dependency/Dependency'
 import { IDependencyPage } from './model/dependencyPage'
 import { PageType } from './model/pageType'
 import { useState } from 'react'
+import ZeroDay from './components/Page/ZeroDay/ZeroDay'
+import { IZeroDayPage } from './model/zeroDayPage'
 
 function App() {
-	const [dependencyData, setDependencyData] = useState({} as IDependencyPage)
-	const [pageType, setPageType] = useState(PageType.Empty as PageType)
+	const [data, setDependencyData] = useState<IDependencyPage | IZeroDayPage>({} as IDependencyPage | IZeroDayPage)
 	window.addEventListener('message', event => {
 		setDependencyData(event.data.data)
-		setPageType(event.data.pageType)
 	})
-	let page = <>Nothing to show</>
-	if (pageType === PageType.Empty) {
-		return page
-	}
+	let page = <></>
 
-	switch (pageType) {
+	switch (data.pageType) {
 		case PageType.Dependency:
-			page = <Dependency data={dependencyData}/>
+			page = <Dependency data={data}/>
 			break
+		case PageType.ZeroDays:
+			page = <ZeroDay data={data}/>
+			break
+		default:
+			page = <>Nothing to show</>
 	}
 
 	return (
