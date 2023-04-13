1a694d1d4f1cd1016ccea8ab09c3d38e30584da0
Or Geva <orgab@jfrog.com>
2023-04-13 10:35:16 +0300

    Fix react-syntax-highlighter to support more languages

--- src/components/UI/MarkDown/MarkDown.tsx
+++ src/components/UI/MarkDown/MarkDown.tsx
@@ -1,5 +1,5 @@
 import ReactMarkdown from 'react-markdown'
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import vscDarkPlus from 'react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus'
 import css from './MarkDown.module.css'
 
