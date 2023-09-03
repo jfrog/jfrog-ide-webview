e13cee9952eeb34be5954baa22d63b9ad717bf24
Shay Dahan <shayd@jfrog.com>
2023-09-03 14:50:43 +0300

    ui changes

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+const esModules = [ // Copy from here 👈
+    "react-markdown",
+    "vfile",
+    "unist-.+",
+    "unified",
+    "bail",
+    "is-plain-obj",
+    "trough",
+    "remark-.+",
+    "mdast-util-.+",
+    "micromark",
+    "parse-entities",
+    "character-entities",
+    "property-information",
+    "comma-separated-tokens",
+    "hast-util-whitespace",
+    "remark-.+",
+    "space-separated-tokens",
+    "decode-named-character-reference",
+    "ccount",
+    "escape-string-regexp",
+    "markdown-table",
+    "trim-lines"
+].join("|")
+
+module.exports = {
+
+}
