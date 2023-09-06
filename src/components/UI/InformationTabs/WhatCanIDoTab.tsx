7ff2b302710c40c3db04c57efaf26a7721b3d46f
Shay Dahan <shayd@jfrog.com>
2023-09-06 13:10:58 +0300

    cr fixes

--- src/components/UI/InformationTabs/WhatCanIDoTab.tsx
+++ src/components/UI/InformationTabs/WhatCanIDoTab.tsx
@@ -5,14 +5,121 @@ import { IImpactGraph, PageType } from '../../../model'
 import Row from '../Row/Row'
 import { ReactComponent as SadFace } from '../../../assets/icons/sad_face.svg'
 import TypographyCss from '../../../styles/Typography.module.css'
+import { eosSuppressExample, secretsSuppressExample } from './texts'
 
-export interface WhatCanIDoTabProps {
+interface Props {
 	pageType: PageType
 	component?: string
 	remediation?: string[]
 	fixedVersion?: string[]
 	impactGraph?: IImpactGraph
 }
+
+function EmptyStateContainer(): JSX.Element {
+	return (
+		<div className={css.emptyStateContainer}>
+			<p>Seems like there is no action you can take at this time</p>
+			<SadFace />
+			<p className={TypographyCss.label}>
+				Consider moving to another package or contact the maintainers to know when a fix will be
+				available
+			</p>
+		</div>
+	)
+}
+
+function UpdateDirectDependency(props: {
+	isDirectDependency: boolean
+	directDependenciesNames: string[]
+	fixedVersion?: string[]
+}): JSX.Element {
+	return (
+		<Collapse
+			header={
+				<h1>
+					{LABELS.UPDATE_THE_DIRECT_DEPENDENCY}
+					<span className={css.recommendedLabel}>Recommended</span>
+				</h1>
+			}
+			content={
+				<div>
+					{props.isDirectDependency ? (
+						<span style={{ display: 'flex', gap: 8, flexDirection: 'column' }}>
+							<Row title="Update the following" data={props.directDependenciesNames.join(', ')} />
+							<Row title="Fix version" data={props.fixedVersion?.join(', ') as string} />
+						</span>
+					) : (
+						<Row title="Update the following" data={props.directDependenciesNames.join(', ')} />
+					)}
+				</div>
+			}
+		/>
+	)
+}
+
+function UpdateIndirectDependency(props: {
+	component: string
+	fixedVersion?: string[]
+}): JSX.Element {
+	return (
+		<Collapse
+			header={<h1>{LABELS.UPDATE_THE_INDIRECT_DEPENDENCY}</h1>}
+			content={
+				<div>
+					<span style={{ display: 'flex', gap: 8, flexDirection: 'column' }}>
+						<Row title="Update the following" data={props.component} />
+						<Row title="Fix version" data={props.fixedVersion?.join(', ') ?? ''} />
+					</span>
+				</div>
+			}
+		/>
+	)
+}
+
+function PatchTheCode(props: { pageType: PageType; remediation: string[] }): JSX.Element {
+	return (
+		<Collapse
+			header={
+				<h1>
+					{LABELS.PATCH_THE_CODE}
+					{[PageType.Eos].includes(props.pageType) && (
+						<span className={css.recommendedLabel}>Recommended</span>
+					)}
+				</h1>
+			}
+			content={
+				<div>
+					<p>
+						<b>{LABELS.REMEDIATION}</b>
+					</p>
+					{props.remediation.map((remediation, index) => (
+						<Markdown text={remediation} key={index} />
+					))}
+				</div>
+			}
+		/>
+	)
+}
+
+function SuppressTheFinding(props: { pageType: PageType }): JSX.Element {
+	const suppressIssueMarkdownExample =
+		props.pageType === PageType.Eos ? eosSuppressExample : secretsSuppressExample
+	return (
+		<Collapse
+			header={<h1>{LABELS.SUPPRESS_THE_FINDING}</h1>}
+			content={
+				<div>
+					{/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+					<p>
+						Add <code>jfrog-ignore</code> comment above the vulnerable line to suppress it
+					</p>
+					<Markdown text={suppressIssueMarkdownExample} />
+				</div>
+			}
+		/>
+	)
+}
+
 export const LABELS = {
 	SUPPRESS_THE_FINDING: 'Suppress the finding',
 	PATCH_THE_CODE: 'Patch the code',
@@ -21,26 +128,14 @@ export const LABELS = {
 	UPDATE_THE_DIRECT_DEPENDENCY: 'Update the direct dependency'
 }
 
-export default function WhatCanIDoTab(props: WhatCanIDoTabProps): JSX.Element {
+export default function WhatCanIDoTab(props: Props): JSX.Element {
 	const isDirectDependency =
 		(props.impactGraph?.root.children ?? []).findIndex(
 			node => node.name.split(':')[0] === props.component
 		) > -1
-	const directDependanciesNames = (props.impactGraph?.root.children ?? []).map(
+	const directDependenciesNames = (props.impactGraph?.root.children ?? []).map(
 		node => node.name.split(':')[0]
 	)
-	const eosSuppressExample = `\`\`\`javascript 
-// Javascript Example 
-export: (req, res) => {
-    res = set_cors(req, res)
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-    payload = Buffer.from(req.body.data, "base64");
-    // jfrog-ignore
-    var data = serialize.unserialize(payload.toString());
-\`\`\``
-	const secretsSuppressExample = '```javascript \n// jfrog-ignore \nconst api_key = "2VTHzn1mKZ..."'
-	const suppressIssueMarkdownExample =
-		props.pageType === PageType.Eos ? eosSuppressExample : secretsSuppressExample
 
 	const hasFixedVersion = props.fixedVersion && props.fixedVersion.length > 0
 	const showSuppressFinding = [PageType.Eos, PageType.Secrets].includes(props.pageType)
@@ -51,86 +146,25 @@ export: (req, res) => {
 			{hasAction ? (
 				<span className={css.text}>Follow one of the following actions:</span>
 			) : (
-				<div className={css.emptyStateContainer}>
-					<p>Seems like there is no action you can take at this time</p>
-					<SadFace />
-					<p className={TypographyCss.label}>
-						Consider moving to another package or contact the maintainers to know when a fix will be
-						available
-					</p>
-				</div>
+				<EmptyStateContainer />
 			)}
 			{hasFixedVersion && (
-				<Collapse
-					header={
-						<h1>
-							{LABELS.UPDATE_THE_DIRECT_DEPENDENCY}
-							<span className={css.recommendedLabel}>Recommended</span>
-						</h1>
-					}
-					content={
-						<div>
-							{isDirectDependency ? (
-								<span style={{ display: 'flex', gap: 8, flexDirection: 'column' }}>
-									<Row title="Update the following" data={directDependanciesNames.join(', ')} />
-									<Row title="Fix version" data={props.fixedVersion?.join(', ') as string} />
-								</span>
-							) : (
-								<Row title="Update the following" data={directDependanciesNames.join(', ')} />
-							)}
-						</div>
-					}
+				<UpdateDirectDependency
+					directDependenciesNames={directDependenciesNames}
+					isDirectDependency={isDirectDependency}
+					fixedVersion={props.fixedVersion}
 				/>
 			)}
 			{props.fixedVersion && props.fixedVersion.length > 0 && !isDirectDependency && (
-				<Collapse
-					header={<h1>{LABELS.UPDATE_THE_INDIRECT_DEPENDENCY}</h1>}
-					content={
-						<div>
-							<span style={{ display: 'flex', gap: 8, flexDirection: 'column' }}>
-								<Row title="Update the following" data={props.component as string} />
-								<Row title="Fix version" data={props.fixedVersion.join(', ')} />
-							</span>
-						</div>
-					}
+				<UpdateIndirectDependency
+					fixedVersion={props.fixedVersion}
+					component={props.component as string}
 				/>
 			)}
 			{props.remediation && props.remediation.length > 0 && (
-				<Collapse
-					header={
-						<h1>
-							{LABELS.PATCH_THE_CODE}
-							{[PageType.Eos].includes(props.pageType) && (
-								<span className={css.recommendedLabel}>Recommended</span>
-							)}
-						</h1>
-					}
-					content={
-						<div>
-							<p>
-								<b>{LABELS.REMEDIATION}</b>
-							</p>
-							{props.remediation.map((remediation, index) => (
-								<Markdown text={remediation} key={index} />
-							))}
-						</div>
-					}
-				/>
+				<PatchTheCode pageType={props.pageType} remediation={props.remediation} />
 			)}
-			{[PageType.Eos, PageType.Secrets].includes(props.pageType) && (
-				<Collapse
-					header={<h1>{LABELS.SUPPRESS_THE_FINDING}</h1>}
-					content={
-						<div>
-							{/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
-							<p>
-								Add <code>jfrog-ignore</code> comment above the vulnerable line to suppress it
-							</p>
-							<Markdown text={suppressIssueMarkdownExample} />
-						</div>
-					}
-				/>
-			)}
+			{showSuppressFinding && <SuppressTheFinding pageType={props.pageType} />}
 		</div>
 	)
 }
