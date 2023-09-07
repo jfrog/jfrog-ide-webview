93f2d70017addb9729e9829770579d6848af1af9
Shay Dahan <shayd@jfrog.com>
2023-09-07 14:07:48 +0300

    ui fixes

--- src/components/Page/Dependency/Navigator/page/Research.tsx
+++ src/components/Page/Dependency/Navigator/page/Research.tsx
@@ -19,7 +19,8 @@ export const LABELS = {
 export default function Research(props: Props): JSX.Element {
 	const increasesRisk = props.data?.jfrogResearchSeverityReason?.filter(v => !v.isPositive)
 	const decreasesRisk = props.data?.jfrogResearchSeverityReason?.filter(v => v.isPositive)
-
+	const hasJfrogResearchSeverityReasons =
+		props.data?.jfrogResearchSeverityReason && props.data.jfrogResearchSeverityReason.length > 0
 	return (
 		<div className={css.container}>
 			<span className={css.alignCenterFlex}>
@@ -50,44 +51,48 @@ export default function Research(props: Props): JSX.Element {
 					</div>
 				</>
 			)}
-			<Divider />
-			<div className={css.group}>
-				<h6>{LABELS.JFROG_RESEARCH_SEVERITY_REASONS}</h6>
-				{increasesRisk && increasesRisk.length > 0 && (
-					<>
-						<span className={`${css.alignCenterFlex} ${css.redText}`}>
-							<MoreRiskIcon /> Increases the risk level:
-						</span>
-						<ul className={css.bulletList}>
-							{increasesRisk.map((reason, index) => (
-								<li key={index}>
-									<p>
-										<b>{reason.name}</b>
-									</p>
-									<Markdown text={reason.description} />
-								</li>
-							))}
-						</ul>
-					</>
-				)}
-				{decreasesRisk && decreasesRisk.length > 0 && (
-					<>
-						<span className={`${css.alignCenterFlex} ${css.greenText}`}>
-							<LessRiskIcon /> Decreases the risk level:
-						</span>
-						<ul className={css.bulletList}>
-							{decreasesRisk.map((reason, index) => (
-								<li key={index}>
-									<p>
-										<b>{reason.name}</b>
-									</p>
-									<Markdown text={reason.description} />
-								</li>
-							))}
-						</ul>
-					</>
-				)}
-			</div>
+			{hasJfrogResearchSeverityReasons && (
+				<>
+					<Divider />
+					<div className={css.group}>
+						<h6>{LABELS.JFROG_RESEARCH_SEVERITY_REASONS}</h6>
+						{increasesRisk && increasesRisk.length > 0 && (
+							<>
+								<span className={`${css.alignCenterFlex} ${css.redText}`}>
+									<MoreRiskIcon /> Increases the risk level:
+								</span>
+								<ul className={css.bulletList}>
+									{increasesRisk.map((reason, index) => (
+										<li key={index}>
+											<p>
+												<b>{reason.name}</b>
+											</p>
+											<Markdown text={reason.description} />
+										</li>
+									))}
+								</ul>
+							</>
+						)}
+						{decreasesRisk && decreasesRisk.length > 0 && (
+							<>
+								<span className={`${css.alignCenterFlex} ${css.greenText}`}>
+									<LessRiskIcon /> Decreases the risk level:
+								</span>
+								<ul className={css.bulletList}>
+									{decreasesRisk.map((reason, index) => (
+										<li key={index}>
+											<p>
+												<b>{reason.name}</b>
+											</p>
+											<Markdown text={reason.description} />
+										</li>
+									))}
+								</ul>
+							</>
+						)}
+					</div>
+				</>
+			)}
 		</div>
 	)
 }
