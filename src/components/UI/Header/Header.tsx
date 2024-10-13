761472609976bcd7d15f857ce24506e0d35b62e5
Bar Vered <161704690+barv-jfrog@users.noreply.github.com>
2024-10-13 12:31:33 +0300

    token-validation (#62)

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -17,6 +17,18 @@ export interface Props {
 	pageData: Partial<IDependencyPage | ISecretsPage | IIaCPage | ISastPage>
 }
 
+const MetadataSpan = (props: { metadata: string }): JSX.Element => (
+	<span className={css.locationLabel}>
+		<b>Token Info:</b> {props.metadata}
+	</span>
+)
+
+const TokenValidationSpan = (props: { tokenValidation: string }): JSX.Element => (
+	<span className={css.locationLabel}>
+		<b>Token Validation:</b> {props.tokenValidation}
+	</span>
+)
+
 const LocationSpan = (props: { location: IAnalysisStep }): JSX.Element => (
 	<span className={css.locationLabel}>
 		<b>Location:</b> {props.location.file}
@@ -67,6 +79,12 @@ export default function Header(props: Props): JSX.Element {
 		(props.pageData as IDependencyPage).extendedInformation !== undefined
 	const applicableData = (props.pageData as IDependencyPage).cve?.applicableData
 
+	const tokenValidation = (props.pageData as ISecretsPage).tokenValidation
+		? (props.pageData as ISecretsPage).tokenValidation
+		: undefined
+	const metadata = (props.pageData as ISecretsPage).metadata
+		? (props.pageData as ISecretsPage).metadata
+		: undefined
 	const ruleId = (props.pageData as ISastPage).ruleId
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	const lineOfVuln = (props.pageData as ISastPage).location
@@ -99,6 +117,8 @@ export default function Header(props: Props): JSX.Element {
 					)}
 					{lineOfVuln && <LineOfVulnSpan lineOfVulnerability={lineOfVuln} />}
 					{ruleId && <RuleIdSpan ruleId={ruleId} />}
+					{tokenValidation && <TokenValidationSpan tokenValidation={tokenValidation} />}
+					{metadata && <MetadataSpan metadata={metadata} />}
 				</div>
 			</div>
 		</div>
